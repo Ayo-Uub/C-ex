7304740583af8d985e69966b43c2ee1053ea6e17
A Y O U B <114445841+Ayo-Uub@users.noreply.github.com>
2024-02-26 23:47:31 +0100

    Add files via upload

---
+++ ex03.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+void ft_print_numbers(void) {
+
+    int numbers = 0 ;
+    while (numbers < 9 )  {
+        numbers ++ ;
+    }
+}
+
+int main ()  {
+
+    ft_print_numbers ();
+}
