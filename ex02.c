d3eb043ad14dec939a5fbac591f009dc4ac5fd3b
A Y O U B <114445841+Ayo-Uub@users.noreply.github.com>
2024-02-26 23:37:44 +0100

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void ft_print_reverse_alphabet(void) {
+    
+    char alphabet = 'z';
+
+    while (alphabet >= 'a')
+    {
+        write (1, &alphabet , 1);
+        alphabet -- ;
+    }
+    
+}
+
+int main () {
+    ft_print_reverse_alphabet ();
+}
